1ed90c7076e287a1f75542603bf716fb02947238
Fabrício <guilhermo.fabricio@gmail.com>
2020-03-26 14:22:56 -0300

    Initial commit

---
+++ integracaoWazeiOS/integracaoWazeiOS/View/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  integracaoWazeiOS
+//
+//  Created by Fabrício Guilhermo on 26/03/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+}
