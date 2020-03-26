c2f0303e20dd857a14313793aba0b162aca2fb0b
Fabrício <guilhermo.fabricio@gmail.com>
2020-03-26 14:57:08 -0300

    Add all the interface elements with autolayout

--- integracaoWazeiOS/integracaoWazeiOS/View/MainViewController.swift
+++ integracaoWazeiOS/integracaoWazeiOS/View/MainViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private lazy var addressTextField: UITextField = {
+        let addressTextField = UITextField(frame: .zero)
+        addressTextField.translatesAutoresizingMaskIntoConstraints = false
+        addressTextField.placeholder = "Destination address"
+        addressTextField.textContentType = .telephoneNumber
+        addressTextField.clearButtonMode = .always
+
+        return addressTextField
+    }()
+
+    private lazy var searchAddressOnWazeButton: UIButton = {
+        let searchAddressOnWazeButton = UIButton(type: .roundedRect)
+        searchAddressOnWazeButton.translatesAutoresizingMaskIntoConstraints = false
+        searchAddressOnWazeButton.setTitle("Traçar rota no Waze", for: .normal)
+        searchAddressOnWazeButton.layer.cornerRadius = 35
+        searchAddressOnWazeButton.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+        searchAddressOnWazeButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        searchAddressOnWazeButton.addTarget(self, action: #selector(searchAddressInWazeButton(_ :)), for: .touchUpInside)
+
+        return searchAddressOnWazeButton
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        view.addSubview(addressTextField)
+        view.addSubview(searchAddressOnWazeButton)
+        autoLayout()
+    }
+
+    @objc private func searchAddressInWazeButton(_ sender: UIButton) {
+        print("actino")
+    }
+}
+
+extension MainViewController {
+    func autoLayout() {
+        addressTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        addressTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20).isActive = true
+        addressTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        addressTextField.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+        searchAddressOnWazeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchAddressOnWazeButton.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        searchAddressOnWazeButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
+        searchAddressOnWazeButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -40).isActive = true
     }
 }
